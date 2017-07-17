536b8fd1a0daf5fc821b11c5ea82c391b87be71c
Lucas Santana <lucasrcsantana@macs-MacBook.local>
2017-07-17 15:37:48 -0300

    HAVING

--- sql-training.sql
+++ sql-training.sql
@@ -77,12 +77,149 @@ GROUP BY
 	1
 ;	
 
-#
+#REVENUE PER TITLE
+#THERE IS AN ERROR IN THE SELECT CLAUSE. WE CAN'T MAKE THE QUERY USING THE KEYWORDS 'F.TITLE', SO I CHANGED FOR 'F.'FILM_ID'. THIS IS BECAUSE A FUNCTION CALLED 'FULL_ORDER_BY' THAT CAN BE DISABLED WHEN THE SERVER STARTS
 SELECT
-
+	f.title as "Film Title", count(r.rental_id), f.rental_rate, count(r.rental_id) * f.rental_rate as revenue
 FROM 
-
+	film f, inventory i, rental r
 WHERE
+	f.film_id = i.film_id AND r.inventory_id = i.inventory_id
+GROUP BY
+	1,3
+ORDER BY 
+	4 DESC
+;
+
+#WHICH CUSTOMER PAID US THE MOST MONEY
+SELECT
+	c.customer_id, c.first_name, c.last_name, sum(p.amount)
+FROM 
+	payment p, customer c
+	WHERE c.customer_id = p.customer_id
+GROUP BY
+	1
+ORDER BY
+	4 DESC
+;	
+
+#WHAT STORE HAS THE HISTORICALLY MOST REVENUE
+SELECT
+	sum(p.amount), c.store_id
+FROM 
+	payment p, customer c
+	WHERE c.customer_id = p.customer_id
+GROUP BY
+	2
+ORDER BY
+	2 DESC
+;	
+
+#HOW MANY RENTALS WE HAVE EACH MONTH
+SELECT
+	left(r.rental_date, 7) , count(r.rental_id)
+FROM 
+	rental r
+GROUP BY
+	1
+ORDER BY
+	2 DESC	
+;	
+
+#HOW MANY RENTALS WE HAVE EACH MONTH
+SELECT
+	f.film_id, f.title, max(r.rental_date), min(r.rental_date)
+FROM 
+	rental r, inventory i, film f
+WHERE
+	i.film_id = f.film_id and r.inventory_id = i.inventory_id
+GROUP BY
+	1
+;	
+
+
+#WHATS CUSTOMERS HAVEN'T RENTED THE MOVIE LAST MONTH
+SELECT
+	r.rental_date, c.first_name, c.last_name
+FROM 
+	customer c, rental r
+WHERE
+	left(r.rental_date, 7) = '2005-05'
+;	
+
+# EVERY CUSTOMER'S LAST RENTAL DATE
+SELECT
+	concat(c.first_name, " ", c.last_name) as Nome, c.email, max(r.rental_date) as "Última data"
+FROM 
+	customer c, rental r
+WHERE
+	c.customer_id = r.customer_id	
+GROUP BY
+	1,2
+;	
+
+#REVENUE BY EACH MONTH
+SELECT
+	left(p.payment_date, 7) as Mês, sum(p.amount) as Revenue
+FROM 
+	payment p
+GROUP BY
+	1
+;	
+
+# HOW MANY DISTINCT RENTS DO WE HAVE PER MONTH
+SELECT
+	left(r.rental_date, 7) as Mês, count(r.rental_id), count(DISTINCT r.customer_id) as "Unique Renters", count(r.rental_id) / count(DISTINCT r.customer_id) as "Rentals per Renter"
+FROM 
+	rental r
+GROUP BY 1
+;	
+
+#NUMBER OF DISTINCT CUSTOMER RENTED EACH FILM IN EACH MONTH
+SELECT
+	f.title, left(r.rental_date, 7) as Mês, count(DISTINCT r.customer_id) as "Unique Films Rented in Month"
+FROM 
+	rental r, inventory i, film f
+WHERE
+	r.inventory_id = i.inventory_id AND i.film_id = f.film_id
+GROUP BY
+	1,2
+;	
+
+#NUMBER OF DISTINCT FILMS RENTED EACH MONTH
+SELECT
+	left(r.rental_date, 7) as Mês, count(r.rental_id), count(DISTINCT i.film_id) as "Distincted films rented by month", count(r.rental_id) / count(DISTINCT i.film_id) as "Avg Rentals per Film"
+FROM 
+	rental r, inventory i
+WHERE
+	r.inventory_id = i.inventory_id
+GROUP BY
+	1
+;	
+
+#NUMBER OF RENTALS IN COMEDY, SPORTS AND FAMILY CATEGORIES
+SELECT
+	c.name, count(r.rental_id)
+FROM 
+	rental r, inventory i, film f, film_category fc, category c
+WHERE
+	r.inventory_id = i.inventory_id 
+	AND i.film_id = f.film_id 
+	AND f.film_id = fc.film_id 
+	AND fc.category_id = c.category_id 
+	AND c.name IN ("Comedy", "Sports", "Family")
+GROUP BY
+	1
+;	
+
+# COMPARISON OPERATORS
+SELECT
+	r.customer_id as Customer, count(r.rental_id) as Rentals
+FROM 
+	rental r
+GROUP BY
+	1
+HAVING count(r.rental_id) >= 3
 	
 ;	
 
@@ -92,6 +229,8 @@ SELECT
 FROM 
 
 WHERE
+
+GROUP BY
 	
 ;	
 
@@ -101,6 +240,8 @@ SELECT
 FROM 
 
 WHERE
+
+GROUP BY
 	
 ;	
 
@@ -110,6 +251,8 @@ SELECT
 FROM 
 
 WHERE
+
+GROUP BY
 	
 ;	
 
@@ -119,6 +262,29 @@ SELECT
 FROM 
 
 WHERE
+
+GROUP BY
 	
 ;	
 
+#
+SELECT
+
+FROM 
+
+WHERE
+
+GROUP BY
+	
+;	
+
+#
+SELECT
+
+FROM 
+
+WHERE
+
+GROUP BY
+	
+;	
